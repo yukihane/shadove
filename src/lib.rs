2744a772ebd29ab4c5c513930bb7db569ab7413d
yukihane <yukihane.feather@gmail.com>
2020-01-02 02:37:02 +0900

    fileからloadする

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ pub mod http;
 use crate::fs::write;
 use crate::http::get;
 
+pub use crate::fs::read as load_from_local;
+
 pub fn fetch_from_remote() {
     let resp = tokio::runtime::Runtime::new().unwrap().block_on(get());
     let cards = resp.unwrap();
