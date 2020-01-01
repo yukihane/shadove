9d0c0429401139ae804ea79fab642ae1a5a04377
yukihane <yukihane.feather@gmail.com>
2020-01-02 02:22:05 +0900

    やはりmainには書かないことにする

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
 pub mod card;
 pub mod fs;
 pub mod http;
+
+use crate::fs::write;
+use crate::http::get;
+
+pub fn fetch_from_remote() {
+    let resp = tokio::runtime::Runtime::new().unwrap().block_on(get());
+    let cards = resp.unwrap();
+    write(cards.as_slice());
+}
--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use shadove::fs::write;
-use shadove::http::get;
-
-fn main() {
-    let resp = tokio::runtime::Runtime::new().unwrap().block_on(get());
-    let cards = resp.unwrap();
-    write(cards.as_slice());
-}
