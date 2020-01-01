8b7ab380444def2ba09c81e2d2cbbd19566d3c83
yukihane <yukihane.feather@gmail.com>
2020-01-01 18:29:06 +0900

    Card構造体にデシリアライズ

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,36 @@
+use card::Card;
+use serde_json::Value;
+use std::collections::HashMap;
+
+mod card;
+
+async fn sample_get() -> Result<(), Box<dyn std::error::Error>> {
+    let v: Value =
+        reqwest::get("https://shadowverse-portal.com/api/v1/cards?format=json&lang=ja&clan=1")
+            .await?
+            .json()
+            .await?;
+
+    let cards = v["data"]["cards"].as_array().unwrap();
+    for x in cards {
+        let obj: Card = serde_json::from_value(x.clone()).unwrap();
+        println!("{:#?}", obj);
+        println!("END");
+    }
+    //    let card = &v["data"]["cards"][0];
+    //    serde_json::from_value(v["data"]["cards"][0]);
+    //    println!("card: {:?}", card);
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let resp = tokio::runtime::Runtime::new()
+            .unwrap()
+            .block_on(sample_get());
+        resp.unwrap();
     }
 }
