916a1d5b2871ae27190e9f3aec34f16b0bd47e9b
yukihane <yukihane.feather@gmail.com>
2020-01-02 01:59:22 +0900

    ファイルから読み取る

--- src/fs.rs
+++ src/fs.rs
@@ -1,9 +1,24 @@
 use crate::card::Card;
 use std::fs::File;
-use std::io::BufWriter;
+use std::io::{BufReader, BufWriter};
 
 pub fn write(cards: &[Card]) {
     let writer = BufWriter::new(File::create("data.bin").unwrap());
 
     serde_json::to_writer_pretty(writer, cards).unwrap();
+}
+
+pub fn read() -> Vec<Card> {
+    let reader = BufReader::new(File::open("data.bin").unwrap());
+    serde_json::from_reader(reader).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        let res = read();
+        println!("{:#?}", res);
+    }
 }
