afccf2c387161888e2d5f16486ad9c183decada3
yukihane <yukihane.feather@gmail.com>
2020-01-03 12:11:14 +0900

    response bodyそのままファイル保存するオプション

--- src/repository.rs
+++ src/repository.rs
@@ -1,23 +1,42 @@
 use crate::card::Card;
+use bytes::Bytes;
 use serde_json::Value;
 use std::fs;
 use std::fs::File;
-use std::io::{BufReader, BufWriter, Error};
+use std::io::{BufReader, BufWriter, Error, Write};
 
 static SAVE_FILE: &str = "data.bin";
 
-pub fn fetch_force() {
-    let json = tokio::runtime::Runtime::new()
-        .unwrap()
-        .block_on(read_value_from_remote())
-        .unwrap();
-    write_value_to_local(&json).unwrap();
+// raw: request-bodyそのままファイル保存するならtrue.
+pub fn fetch_force(raw: bool) {
+    if raw {
+        let json = tokio::runtime::Runtime::new()
+            .unwrap()
+            .block_on(read_raw_value_from_remote())
+            .unwrap();
+        write_raw_value_to_local(&json).unwrap();
+    } else {
+        let json = tokio::runtime::Runtime::new()
+            .unwrap()
+            .block_on(read_value_from_remote())
+            .unwrap();
+        write_value_to_local(&json).unwrap();
+    }
 }
 
 async fn read_value_from_remote() -> Result<Value, Box<dyn std::error::Error>> {
     let v: Value = reqwest::get("https://shadowverse-portal.com/api/v1/cards?format=json&lang=ja")
         .await?
         .json()
+        .await?;
+
+    Ok(v)
+}
+
+async fn read_raw_value_from_remote() -> Result<Bytes, Box<dyn std::error::Error>> {
+    let v = reqwest::get("https://shadowverse-portal.com/api/v1/cards?format=json&lang=ja")
+        .await?
+        .bytes()
         .await?;
 
     Ok(v)
@@ -32,6 +51,14 @@ fn write_value_to_local(json: &Value) -> Result<(), Error> {
     Ok(())
 }
 
+fn write_raw_value_to_local(bytes: &Bytes) -> Result<(), Error> {
+    let file = File::create(SAVE_FILE)?;
+    let mut writer = BufWriter::new(file);
+
+    writer.write_all(bytes)?;
+
+    Ok(())
+}
 pub fn find_all() -> Result<Vec<Card>, Error> {
     let file = File::open(SAVE_FILE)?;
     let reader = BufReader::new(file);
